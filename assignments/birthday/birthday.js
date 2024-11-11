cb162b6900d74f4e453043954cf1c9b756f523cf
Andrew Kim <andrewkimsvhs@gmail.com>
2024-11-10 22:05:21 -0800

    notes and assignment

--- assignments/birthday/birthday.js
+++ assignments/birthday/birthday.js
@@ -1,16 +1,39 @@
+    let months = form.months.value;
+    let days = form.days.value;
+
 const today = new Date();
 const month = today.getMonth();
 const day = today.getDate();
 
 
-const bday = new Date(1997, 2, 5) // march is 2 because of array indexing.
+const bday = new Date(months, days) //const bday = new Date(1997, 2, 5) march is 2 because of array indexing.
 const bd_month = bday.getMonth();
 const bd_day = bday.getDate();
 
+const submit = document.getElementById("submit"); 
+message.style.display="none";
+
+submit.addEventListener("click", itsYourBirthday);
+
+form.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        generatePoem();
+    }
+});
+
+function itsYourBirthday(){
+
+
 if(bd_month==month && bd_day == day){
-    console.log("Happy Birthday!")
+    document.getElementById("message").innerHTML = "Happy Birthday!"; 
+
 }
 else{
-    console.log("It's not your birthday")
+    document.getElementById("message").innerHTML = "It's not your birthday today"; 
+
+}
+
+
 }
 
