9cc48ba4a10e8963d5968d16b711becda8b6de68
Andrew Kim <84065006+andrew001347@users.noreply.github.com>
2024-12-04 20:59:26 -0800

    final

---
+++ assignments/final_Project/script.js
@@ -0,0 +1,20 @@
+
+const my_form = document.forms.sayHello; 
+const message = document.getElementById("message");
+
+function display(){
+    let awardee = document.getElementsByTagName("name").value;
+
+dispWin = window.open('certificate.html', 'NewWin',
+    'toolbar=no,status=no,titlebar=no,width=800,height=600')
+
+// send the name over to certificate.html
+awardee = document.form1.name.value;
+
+
+
+// reset the value
+document.form1.name.value = "";
+
+}
+
