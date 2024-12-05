9ff3862ccbe00ac20dd94d7247a7136e2a3fc763
Andrew Kim <84065006+andrew001347@users.noreply.github.com>
2024-12-04 21:39:05 -0800

    coloring

--- assignments/final_Project/script.js
+++ assignments/final_Project/script.js
@@ -1,18 +1,17 @@
 
-const my_form = document.forms.sayHello; 
-const message = document.getElementById("message");
+const my_form = document.forms.cert; 
+const printName = document.getElementsByClassName("awardee");
+var awardee = window.opener.awardee;
 
 function display(){
     let awardee = document.getElementsByTagName("name").value;
 
-dispWin = window.open('certificate.html', 'NewWin',
-    'toolbar=no,status=no,titlebar=no,width=800,height=600')
+dispWin = window.open('certificate.html', 'NewWin','toolbar=no,status=no,titlebar=no,width=800,height=600')
 
 // send the name over to certificate.html
 awardee = document.form1.name.value;
 
-
-
+document.write(awardee);
 // reset the value
 document.form1.name.value = "";
 
